c2f48ddc9430245adf1afb32a05604a9b29a688e
Jiří Veselka <jiri.veselka@hotmail.com>
2016-03-06 22:56:42 +0100

    Referenced Font-Awesome via Bower

--- ObscureDesign.Management/Controllers/UserController.cs
+++ ObscureDesign.Management/Controllers/UserController.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using ObscureDesign.Data;
+using ObscureDesign.Management.Models;
 
 namespace ObscureDesign.Management.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Create()
+        [FromServices]
+        public AppDbContext Context { get; set; }
+
+        public IActionResult List()
+        {
+            var model = Context.Users.ToViewModel();
+            return View(model);
+        }
+
+        public IActionResult Edit(int id)
         {
             return View();
         }
---
+++ ObscureDesign.Management/Models/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ObscureDesign.Management.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+    }
+
+    public static class UserModelExtensions
+    {
+        public static IEnumerable<UserModel> ToViewModel(this IQueryable<Data.User> source)
+        {
+            return source.Select(user => new UserModel
+            {
+                Id = user.UserId,
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+            });
+        }
+    }
+}
